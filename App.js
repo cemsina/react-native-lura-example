5af5e1d36867eedc774bb914ef3d45f3ae0e012a
Cemsina Güzel <cemsina_guzel@alyo.com.tr>
2022-11-15 02:49:33 +0300

    initial

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { LuraPlayer } from "react-native-lura";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LuraPlayer
+        accessKey="wyJe6YlbNfdbkElfeKfo3NGas8Dbdn48"
+        video="3973918"
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2aWQiOiIzOTczOTE4IiwiaXNzIjoid3lKZTZZbGJOZmRia0VsZmVLZm8zTkdhczhEYmRuNDgifQ.0mpWd0_5hSbNs2Th_vwZXGBtn3dN1HmvxQciR0UFPU8"
+        style={styles.player}
+      />
     </View>
   );
 }
@@ -14,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 200,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  player: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "#fff",
   },
 });
