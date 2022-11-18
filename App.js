b03830000475b865a35a31575517ea4c77353758
Cemsina Güzel <cemsina_guzel@alyo.com.tr>
2022-11-18 21:08:02 +0300

    changed package

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { LuraPlayer } from "react-native-lura";
+import { LuraPlayer } from "lura-react-native-core-alpha";
 
 export default function App() {
   return (
